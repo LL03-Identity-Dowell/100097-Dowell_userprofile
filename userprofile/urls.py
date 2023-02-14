a833effaa695a2d3efcba8202b9904169e363a7b
Emmanuel Ugwu <95964539+Nuel1st@users.noreply.github.com>
2023-02-14 21:46:11 +0100

    I updated the views and also added urls to each of the variables
    views and urls

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from userprofile.views import *
+from .views import serverReport, insert_data
 
 urlpatterns = [
-    path('',serverReport.as_view()),
+    path('server-report/', serverReport.as_view(), name='server_report'),
+    path('insert-data/', insert_data, name='insert_data'),
+    path('first-name/', FirstnameView.as_view(), name='first_name'),
+    path('last-name/', LastnameView.as_view(), name='last_name'),
+    path('phone-number/', PhoneNumberView.as_view(), name='phone_number'),
+    path('email-address/', EmailAddressView.as_view(), name='email_address'),
+    path('address/', AddressView.as_view(), name='address'),
+    path('pincode/', PincodeView.as_view(), name='pincode'),
+    path('location/', LocationView.as_view(), name='location'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,9 +1,42 @@
-import json
-from django.http import HttpResponse ,JsonResponse
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+class FirstnameView(View):
+    def get(self, request):
+        return HttpResponse()
+
+class LastnameView(View):
+    def get(self, request):
+        return HttpResponse()
+
+class PhoneNumberView(View):
+    def get(self, request):
+        return HttpResponse()
+
+class EmailAddressView(View):
+    def get(self, request):
+        return HttpResponse()
+
+class AddressView(View):
+    def get(self, request):
+        return HttpResponse()
+
+class PincodeView(View):
+    def get(self, request):
+        return HttpResponse()
+
+class LocationView(View):
+    def get(self, request):
+        return HttpResponse()
+
+from django.shortcuts import render
 import requests
 import json
+import datetime
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,6 +44,58 @@ from rest_framework.views import APIView
 @method_decorator(csrf_exempt, name='dispatch')
 class serverReport(APIView):
     def get(self, request):
-        return Response({"status": "Server is working."}, status= status.HTTP_200_OK)
+        return Response({"status": "Server is working."}, status=status.HTTP_200_OK)
 
+def insert_data(request):
+    url = "http://100002.pythonanywhere.com/"
 
+    payload = json.dumps({
+        "cluster": "login",
+        "database": "login",
+        "collection": "user_profile",
+        "document": "user_profile",
+        "team_member_ID": "1168",
+        "function_ID": "ABCDE",
+        "command": "insert",
+        "field": {
+            "profile": {
+                "first_name": "First Name",
+                "last_name": "Last Name",
+                "phone number": "Phone",
+                "email": "email address",
+                "address": "address",
+                "pincode": "pincode",
+                "location": "location"
+            },
+        },
+        "update_field": {
+            "order_nos": 21
+        },
+        "platform": "bangalore"
+    })
+
+    data_dic = json.loads(payload)
+
+    first_name = data_dic['field']['profile']['first_name']
+    last_name = data_dic['field']['profile']['last_name']
+    phone = data_dic['field']['profile']['phone number']
+    email_address = data_dic['field']['profile']['email']
+    address = data_dic['field']['profile']['address']
+    pincode = data_dic['field']['profile']['pincode']
+    location = data_dic['field']['profile']['location']
+
+    context = {
+        'first_name': first_name, 
+        'last_name': last_name,
+        'phone_number': phone,
+        'email_address': email_address,
+        'address': address,
+        'pincode': pincode,
+        'location': location
+    }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    return render(request, 'insert_data.html', context=context)
