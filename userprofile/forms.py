ebe06231a2d243ff372875b4c282738e28c3f66d
Emmanuel Ugwu <95964539+Nuel1st@users.noreply.github.com>
2023-03-06 04:42:56 +0100

    updated

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -1,5 +1,5 @@
 from django import forms
- 
+from django.contrib.auth.forms import PasswordChangeForm
 # creating a form
 class InputForm(forms.Form):
  
@@ -10,5 +10,20 @@ class InputForm(forms.Form):
     address = forms.CharField(max_length= 200)
     pincode = forms.CharField(max_length= 200)
     location = forms.CharField(max_length= 200)
+    # password = forms.CharField( max_length=100)
 
-    
+class InputForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+class OrganizationForm(forms.Form):
+    Your_Organization_Name = forms.CharField(max_length=100)
+    Organization_Address = forms.CharField(max_length=100)
+    PINZIPCODE = forms.CharField(max_length=100)
+    city_of_your_Organization = forms.CharField(max_length=100)
+    country_of_your_organization = forms.CharField(max_length=100)
+    Organization_logo =forms.CharField(max_length=100)
+    Upload_new_logo = forms.ImageField()
+    Latitude_of_Organization = forms.CharField(max_length=100)
+    Longitude_of_Organization = forms.CharField(max_length=100)
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
-from userprofile. views import *
-# from .views import serverReport, insert_data
-app_name = "base"
+from views import *
+# from .views import insert_data
+app_name = "core"
+
+    # path('server-report/', serverReport.as_view(), name='server_report')
 
 urlpatterns = [
-    # path('server-report/', serverReport.as_view(), name='server_report'),
-    path('insert_data', insert_data, name='insert_data'),
-    path('first-name/', FirstnameView.as_view(), name='first_name'),
-    path('last-name/', LastnameView.as_view(), name='last_name'),
-    path('phone-number/', PhoneNumberView.as_view(), name='phone_number'),
-    path('email-address/', EmailAddressView.as_view(), name='email_address'),
+    path('firstname/', FirstnameView.as_view(), name='firstname'),
+    path('lastname/', LastnameView.as_view(), name='lastname'),
+    path('phonenumber/', PhoneNumberView.as_view(), name='phonenumber'),
+    path('emailaddress/', EmailAddressView.as_view(), name='emailaddress'),
     path('address/', AddressView.as_view(), name='address'),
     path('pincode/', PincodeView.as_view(), name='pincode'),
     path('location/', LocationView.as_view(), name='location'),
+    path('set-password/', set_password, name='set_password'),
+    path('organization/', organization_view, name='organization')
 ]
+
+
+
