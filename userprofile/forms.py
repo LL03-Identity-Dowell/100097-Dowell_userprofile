42779dd2c91ccaacf7dd45291532c5ab96d6be85
deepaktho <deepakthorat900@gmail.com>
2023-03-08 11:36:22 +0530

    .

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -2,7 +2,7 @@ from django import forms
  
 # creating a form
 class InputForm(forms.Form):
- 
+    edit_inputform = forms.BooleanField(widget=forms.HiddenInput, initial=True)
     first_name = forms.CharField(max_length = 200)
     last_name = forms.CharField(max_length = 200)
     phone_number = forms.CharField(max_length= 200)
@@ -11,4 +11,10 @@ class InputForm(forms.Form):
     pincode = forms.CharField(max_length= 200)
     location = forms.CharField(max_length= 200)
 
+class DeviceIdForm(forms.Form):
+    edit_deviceidform = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    phone_id = forms.CharField(max_length = 200)
+    brand_model = forms.CharField(max_length = 200)
+    laptop_model = forms.CharField(max_length= 200)
+    tablet_model = forms.EmailField(max_length= 200)
     
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from .forms import InputForm
+from .forms import InputForm,DeviceIdForm
 
     # return render(request, insert_data.html, {'form': form})
 
@@ -51,57 +51,124 @@ from django.views.decorators.csrf import csrf_exempt
 #     def get(self, request):
 #         return Response({"status": "Server is working."}, status=status.HTTP_200_OK)
 
+def dowellconnection(cluster,platform,database,collection,document,team_member_ID,function_ID,command,field,update_field):
+    url = 'http://100002.pythonanywhere.com/'
+    data= json.dumps({
+      "cluster": cluster,
+      "platform": platform,
+      "database": database,
+      "collection": collection,
+      "document": document,
+      "team_member_ID": team_member_ID,
+      "function_ID": function_ID,
+      "command": command,
+      "field": field,
+      "update_field":update_field
+       })
+    headers = {'content-type': 'application/json'}
+    response = requests.request('POST',url, headers=headers,data=data)
+    return response.text
+
+def get_user_object(username):
+    fields = {"usename": username}
+    response_obj = dowellconnection( "login", "login", "user_profile", "user_profile", "1168", "ABCDE", "find", fields, "nil")
+    res_obj = json.loads(response_obj)
+    try:
+        return res_obj["data"]
+    except:
+        return []
+
 def user_profile(request):
     url = "http://100002.pythonanywhere.com/"
 
     if request.method == 'POST':
-        form = InputForm(request.POST)
-        if form.is_valid():      
-            first_name=form.cleaned_data['first_name']
-            last_name=form.cleaned_data['last_name']
-            phone_number=form.cleaned_data['phone_number']
-            email_address=form.cleaned_data['email_address']
-            address=form.cleaned_data['address']
-            pincode=form.cleaned_data['pincode']
-            location=form.cleaned_data['location']
         
-            payload = json.dumps({
-            "cluster": "login",
-            "database": "login",
-            "collection": "user_profile",
-            "document": "user_profile",
-            "team_member_ID": "1168",
-            "function_ID": "ABCDE",
-            "command": "insert",
-            "field": {
-                "profile": {
-                    "first_name": first_name,
-                    "last_name": last_name,
-                    "phone number": phone_number,
-                    "email": email_address,
-                    "address": address,
-                    "pincode": pincode,
-                    "location": location
+        if 'edit_inputform' in request.POST:
+            form = InputForm(request.POST)
+            if form.is_valid():      
+                first_name=form.cleaned_data['first_name']
+                last_name=form.cleaned_data['last_name']
+                phone_number=form.cleaned_data['phone_number']
+                email_address=form.cleaned_data['email_address']
+                address=form.cleaned_data['address']
+                pincode=form.cleaned_data['pincode']
+                location=form.cleaned_data['location']
+                
+                payload = json.dumps({
+                "cluster": "login",
+                "database": "login",
+                "collection": "user_profile",
+                "document": "user_profile",
+                "team_member_ID": "1168",
+                "function_ID": "ABCDE",
+                "command": "update",
+                "field": {
+                    #identify username or id
+                    
                 },
-            },
-            "update_field": {
-                "order_nos": 21
-            },
-            "platform": "bangalore"
-            })
+                "update_field": {
+                    "order_nos": 21,
+                    "profile": {
+                        "first_name": first_name,
+                        "last_name": last_name,
+                        "phone number": phone_number,
+                        "email": email_address,
+                        "address": address,
+                        "pincode": pincode,
+                        "location": location
+                    },
+                },
+                "platform": "bangalore"
+                })
 
-            headers = {
-            'Content-Type': 'application/json'
-            }
+                headers = {
+                'Content-Type': 'application/json'
+                }
 
-            response = requests.request("POST", url, headers=headers, data=payload)
-            print(response.text)
-            print("hi")
+                response = requests.request("POST", url, headers=headers, data=payload)
+                print(response.text)
+                print("hi")
+        if 'edit_deviceidform' in request.POST:
+            device_id_form = DeviceIdForm(request.POST)
+            if  device_id_form.is_valid():
+                phone_id = device_id_form.cleaned_data['phone_id']
+                brand_model = device_id_form.cleaned_data['brand_model']
+                laptop_model = device_id_form.cleaned_data['laptop_model']
+                tablet_model = device_id_form.cleaned_data['tablet_model']
+                payload = json.dumps({
+                "cluster": "login",
+                "database": "login",
+                "collection": "user_profile",
+                "document": "user_profile",
+                "team_member_ID": "1168",
+                "function_ID": "ABCDE",
+                "command": "update",
+                "field": {
+                    
+                },
+                "update_field": {
+                    "order_nos": 21
+                },
+                "platform": "bangalore"
+                })
+
+                headers = {
+                'Content-Type': 'application/json'
+                }
+
+                response = requests.request("POST", url, headers=headers, data=payload)
+                print("device_id_form")
+                print(" ")
+                print("ghfjdksal")
         
         # data_dic = json.loads(payload)
         # f = data_dic['field']['profile']['first_name']
-        return render(request, "index.html",)            
+        context ={} 
+        context['form']= InputForm()
+        context['device_id']=DeviceIdForm()
+        return render(request, "index.html",context)            
     else:
         context ={} 
         context['form']= InputForm()
+        context['device_id']=DeviceIdForm()
         return render(request, "index.html", context)
