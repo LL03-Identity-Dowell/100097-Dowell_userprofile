b8551ed5ba46e0bdc7f8b09130519c165766d4ad
Nagarajuvuliki <87876269+Nagarajuvuliki@users.noreply.github.com>
2023-12-08 15:55:22 +0530

    add new react files

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,6 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -156,4 +155,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATIC_URL = 'images/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'build/images'),
+    #os.path.join(os.path.join(BASE_DIR, 'build'), 'static', 'images'),
 )
