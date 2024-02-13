e30a42310da94e968c25e170337a90e3d750155f
=HiraZahid98 <=hira43368@gmail.com>
2024-02-13 16:08:41 +0500

    Idverification accordion added

---
+++ src/components/AccordionComponents/IdVerificationView.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Col, ListGroup, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+const IDVerification = () => {
+	const allidverifications = useSelector((state) => state.idverify);
+
+	return (
+		<>
+			<ListGroup as="ul">
+				{Object.entries(allidverifications).map(
+					([idVerifyKey, idverifyValue], index) => (
+						<ListGroup.Item as="li" key={index}>
+							<div className="ms-2 me-auto">
+								<Row className="align-items-center">
+									<Col sm={3} className="fw-bold">
+										{idVerifyKey}
+									</Col>
+									<Col sm={9}>
+										{idverifyValue}
+									</Col>
+								</Row>
+							</div>
+						</ListGroup.Item>
+					)
+				)}
+			</ListGroup>
+		</>
+	);
+};
+
+export default IDVerification;
