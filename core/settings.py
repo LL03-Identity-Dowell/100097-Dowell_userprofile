a38c374302146e099a1eca48ccde552487c17fe5
Amit KC <amit.kc240@gmail.com>
2024-02-16 20:44:53 +0545

    hardcode get users voiceID passcode

--- core/settings.py
+++ core/settings.py
@@ -1,31 +1,49 @@
+# from pathlib import Path
+# import os
+# import platform
+# from django.conf import settings
+# from datetime import timedelta
+
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# # python-dotenv
+# from dotenv import load_dotenv
+
+# system = platform.system()
+
+# if system == "Windows":
+#     load_dotenv()
+# elif system == "Linux":
+#     env_location = os.path.join(BASE_DIR, ".env")
+#     load_dotenv(env_location)
+
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# GET_ALL_USERS_VOICEID_PASSCODE = os.getenv("Get_All_Users_VoiceID_PASSCODE")
+
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = os.getenv("DEBUG") == "True"
+
+# ALLOWED_HOSTS = ["*"]
+
 from pathlib import Path
 import os
-import platform
-from django.conf import settings
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# python-dotenv
-from dotenv import load_dotenv
 
-system = platform.system()
-
-if system == "Windows":
-    load_dotenv()
-elif system == "Linux":
-    env_location = os.path.join(BASE_DIR, ".env")
-    load_dotenv(env_location)
-
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-GET_ALL_USERS_VOICEID_PASSCODE = os.getenv("Get_All_Users_VoiceID_PASSCODE")
-
+SECRET_KEY = "django-insecure-1p0fbio&__*vir%eu3n!ip3z(_r56z7pv&$!5qa#c0zvo^ow8a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -33,59 +51,57 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'userprofile.apps.UserprofileConfig',
-    'rest_framework',
-    'core',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "userprofile.apps.UserprofileConfig",
+    "rest_framework",
+    "core",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'react')
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "react")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -95,16 +111,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -112,21 +128,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
 
 from django.contrib.messages import constants as messages
+
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
 }
 
 # Static files (CSS, JavaScript, Images)
@@ -136,29 +153,29 @@ MESSAGE_TAGS = {
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = True
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 CORS_ALLOW_ALL_ORIGINS = True
 
 
-STATIC_URL = '/images/'
+STATIC_URL = "/images/"
 
 if DEBUG:
     # STATICFILES_DIRS = [
     #     os.path.join(BASE_DIR,'react/images'),
     # ]
-   
-    #STATIC_ROOT="E:\assign\gitprojects\newuserprofile\100097-Dowell_userprofile\react\images"
-    STATIC_ROOT = os.path.join(BASE_DIR,'react/images')
-    
-    
+
+    # STATIC_ROOT="E:\assign\gitprojects\newuserprofile\100097-Dowell_userprofile\react\images"
+    STATIC_ROOT = os.path.join(BASE_DIR, "react/images")
+
+
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR,'react/images')
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'frontend/static'),
-    #os.path.join(os.path.join(BASE_DIR, 'build'), 'static', 'images'),
-#)
+    STATIC_ROOT = os.path.join(BASE_DIR, "react/images")
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+# os.path.join(BASE_DIR, 'frontend/static'),
+# os.path.join(os.path.join(BASE_DIR, 'build'), 'static', 'images'),
+# )
--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.conf import settings
 
+
 class SectionSerializer(serializers.Serializer):
     Section_Name = serializers.CharField()
     MyProfile = serializers.BooleanField(required=False)
@@ -17,7 +18,12 @@ class SectionSerializer(serializers.Serializer):
     UsageProfile = serializers.BooleanField(required=False)
 
 
-IdVerification_Choices = ["Rejected_After_Verification", "Not_Started", "In_Progress","Accepted_After_Verification"]
+IdVerification_Choices = [
+    "Rejected_After_Verification",
+    "Not_Started",
+    "In_Progress",
+    "Accepted_After_Verification",
+]
 
 
 class IdVerificationSerializer(serializers.Serializer):
@@ -34,8 +40,12 @@ class IdVerificationSerializer(serializers.Serializer):
     idCard5_Verification = serializers.ChoiceField(choices=IdVerification_Choices)
     signature_Verification = serializers.ChoiceField(choices=IdVerification_Choices)
     socialMedia_Verification = serializers.ChoiceField(choices=IdVerification_Choices)
-    personalReference_Verification = serializers.ChoiceField(choices=IdVerification_Choices)
-    personal_Verification_By_Witness = serializers.ChoiceField(choices=IdVerification_Choices)
+    personalReference_Verification = serializers.ChoiceField(
+        choices=IdVerification_Choices
+    )
+    personal_Verification_By_Witness = serializers.ChoiceField(
+        choices=IdVerification_Choices
+    )
     organisation_Verification = serializers.ChoiceField(choices=IdVerification_Choices)
 
 
@@ -46,18 +56,26 @@ class UserSerializer(serializers.Serializer):
     idverification = IdVerificationSerializer(required=False)
 
     def validate(self, attrs):
-        if 'section' not in attrs and 'idverification' not in attrs:
-            raise serializers.ValidationError("Either 'section' or 'idverification' should be provided.")
-        if 'section' in attrs and 'idverification' in attrs:
-            raise serializers.ValidationError("Both 'section' or 'idverification' cant be updated at one.")
+        if "section" not in attrs and "idverification" not in attrs:
+            raise serializers.ValidationError(
+                "Either 'section' or 'idverification' should be provided."
+            )
+        if "section" in attrs and "idverification" in attrs:
+            raise serializers.ValidationError(
+                "Both 'section' or 'idverification' cant be updated at one."
+            )
         return attrs
+
+
+server_passcode = "login123"
 
 
 class GetAllUsersVoiceIDSerializer(serializers.Serializer):
     passcode = serializers.CharField(required=True)
 
     def validate(self, attrs):
-        req_passcode = attrs.get('passcode')
-        if req_passcode != settings.GET_ALL_USERS_VOICEID_PASSCODE:
+        req_passcode = attrs.get("passcode")
+        # if req_passcode != settings.GET_ALL_USERS_VOICEID_PASSCODE:
+        if req_passcode != server_passcode:
             raise serializers.ValidationError("Invalid Passcode!")
         return attrs
